f6ebd007d953b6f950a626ed918d14e396e55ab1
theox443 <theo199323@gmail.com>
2025-02-13 03:55:19 +0700

    Create config.php

---
+++ php/config.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$db = 'toko_kami';
+$user = 'root';
+$pass = '';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
